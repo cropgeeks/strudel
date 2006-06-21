953036e31d8ab343f042b1c94eed7d6069f768d4
Iain Milne <imilne@users.noreply.github.com>
2006-06-21 13:13:14 +0000

    Renamed Map.java to ChromoMap.java. Fixed Iterator looping for MapSet and ChromoMap. Added package io. Added io.JoinMapImporter.java to test read data in fudged JoinMap format.

--- src/sbrn/mapviewer/data/MapSet.java
+++ src/sbrn/mapviewer/data/MapSet.java
@@ -4,13 +4,18 @@ import java.util.*;
 
 // So far this class is just a wrapper around a list (with a name). Overkill for
 // now but it may require more in the future.
-public class MapSet implements Iterable
+public class MapSet implements Iterable<ChromoMap>
 {
 	// The name of this MapSet
 	private String name;
 	
 	// Holds a list of all the maps in this set
-	private LinkedList<Map> maps = new LinkedList<Map>();
+	private LinkedList<ChromoMap> maps = new LinkedList<ChromoMap>();
+	
+	public MapSet()
+	{
+		name = "";
+	}
 		
 	public MapSet(String name)
 	{
@@ -19,7 +24,7 @@ public class MapSet implements Iterable
 	
 	// Allows you to use MapSet in a J2SE1.5 for loop:
 	// for (Map map: myMapSet) {}
-	public Iterator<Map> iterator()
+	public Iterator<ChromoMap> iterator()
 		{ return maps.iterator(); }
 	
 	public String getName()
@@ -28,10 +33,10 @@ public class MapSet implements Iterable
 	public void setName(String name)
 		{ this.name = name; }
 	
-	public LinkedList<Map> getMaps()
-		{ return maps; }
+	public void addMap(ChromoMap map)
+		{ maps.add(map); }
 	
-	public Map getMap(int index)
+	public ChromoMap getMap(int index)
 		{ return maps.get(index); }
 	
 	public int size()

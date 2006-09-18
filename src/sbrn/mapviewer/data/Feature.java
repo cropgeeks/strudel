ebe47371f1b9ae0ac14a2b8452cd69e7b7a8109a
Iain Milne <imilne@users.noreply.github.com>
2006-09-18 15:12:10 +0000

    Javadoc updates to the data API.

--- src/sbrn/mapviewer/data/Feature.java
+++ src/sbrn/mapviewer/data/Feature.java
@@ -2,6 +2,11 @@ package sbrn.mapviewer.data;
 
 import java.util.*;
 
+/**
+ * Class that represents a "feature" on a map. This is rather abstract for now,
+ * so a Feature object can literally correspond to whatever "feature" is
+ * required for a given task - marker, qtl, annotation, etc.
+ */
 public class Feature
 {
 	// Feature types
@@ -15,27 +20,39 @@ public class Feature
 	// Owning ChromoMap for this Feature
 	private ChromoMap cMap;
 	
-	// The name of this feature
+	// The name of this Feature
 	private String name;
 	// And any aliases
 	private LinkedList<String> aliases = new LinkedList<String>();
+	// And any links its involved in
+	private LinkedList<Link> links = new LinkedList<Link>();
 	
 	// Its start and stop positions (in whatever distance format) on the map
 	private float start, stop;
-	
-	public double rnd = Math.random();
-	
+		
 	// Feature type
 	private int type = GENERIC;
 	
+	/**
+	 * Constructs a new feature with the given name.
+	 * @param name the name of this feature
+	 */
 	public Feature(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * Returns a string representation of this feature. Currently its name.
+	 * @return a string representation of this feature.
+	 */
 	public String toString()
 		{ return name; }
 	
+	/**
+	 * Returns the name of this feature.
+	 * @return the name of this feature
+	 */
 	public String getName()
 		{ return name; }
 	
@@ -57,15 +74,40 @@ public class Feature
 	public void setType(int type)
 		{ this.type = type; }
 	
+	/**
+	 * Returns a list of every alias that this feature is known by.
+	 * @return a list of every alias that this feature is known by
+	 */
 	public LinkedList<String> getAliases()
 		{ return aliases; }
 	
+	/**
+	 * Adds another name alias for this feature.
+	 * @param alias the name alias to add
+	 */
 	public void addAlias(String alias)
 		{ aliases.add(alias); }
 	
+	/**
+	 * Returns a reference to the ChromoMap object that contains this feature.
+	 * @return a reference to the ChromoMap object that contains this feature.
+	 */
 	public ChromoMap getOwningMap()
 		{ return cMap; }
 	
+	/**
+	 * Sets the owning ChromoMap object for this feature.
+	 * @param cMap the new owning ChromoMap object for this feature
+	 */
 	public void setOwningMap(ChromoMap cMap)
 		{ this.cMap = cMap; }
+	
+	/**
+	 * Returns a list of links that correspond to this feature. Each Link will
+	 * hold a reference to this feature in either its feature1 or feature2
+	 * reference.
+	 * @return a list of links that correspond to this feature.
+	 */
+	public LinkedList<Link> getLinks()
+		{ return links; }
 }
--- src/sbrn/mapviewer/data/LinkSet.java
+++ src/sbrn/mapviewer/data/LinkSet.java
@@ -3,7 +3,7 @@ package sbrn.mapviewer.data;
 import java.util.*;
 
 /**
- * Holds a list of Links between Features. The LinkSet does not concern itself
+ * Holds a list of links between features. The link set does not concern itself
  * with where the features are actually located, but it can contain a list of
  * one or more MapSet objects that relate to where its feature links are valid.
  */
@@ -13,28 +13,51 @@ public class LinkSet
 	
 	private LinkedList<MapSet> mapSets = new LinkedList<MapSet>();
 	
+	/**
+	 * Constructs a new LinkSet.
+	 */
 	public LinkSet()
 	{
 	}
 	
+	/**
+	 * Returns the list of links held by this object.
+	 * @return the list of links held by this object
+	 */
 	public LinkedList<Link> getLinks()
 		{ return links; }
 	
+	/**
+	 * Returns the list of map sets held by this object.
+	 * @return the list of map sets held by this object
+	 */
 	public LinkedList<MapSet> getMapSets()
 		{ return mapSets; }
 	
-	/** Adds another MapSet to this object's collection of sets. */
+	/**
+	 * Adds another map set to this link set.
+	 * @param mapset the map set to add
+	 */
 	public void addMapSet(MapSet mapset)
 	{
 		if (mapSets.contains(mapset) == false)
 			mapSets.add(mapset);
 	}
 	
+	/**
+	 * Adds another link to this link set.
+	 * @param link the link to add
+	 */
 	public void addLink(Link link)
 	{
 		links.add(link);
 	}
 	
+	/**
+	 * Returns a string representation of this link set. The string is formatted
+	 * to return details on each link; one per line.
+	 * @return a string representation of this link set
+	 */
 	public String toString()
 	{
 		StringBuffer str = new StringBuffer();

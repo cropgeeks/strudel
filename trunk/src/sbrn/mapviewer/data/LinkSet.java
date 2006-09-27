5aa3afafb9d23ca6cbcbed89577f095b439e7aec
imilne <imilne@75c49e0a-a916-0410-83a6-e98dbbb37169>
2006-09-27 15:08:38 +0000

    Started adding more powerful methods to LinkSet.java so that various searches are possible and sub-link-sets can be returned containing the search results.

--- trunk/src/sbrn/mapviewer/data/LinkSet.java
+++ trunk/src/sbrn/mapviewer/data/LinkSet.java
@@ -7,7 +7,7 @@ import java.util.*;
  * with where the features are actually located, but it can contain a list of
  * one or more MapSet objects that relate to where its feature links are valid.
  */
-public class LinkSet
+public class LinkSet implements Iterable<Link>
 {
 	private LinkedList<Link> links = new LinkedList<Link>();
 	
@@ -28,11 +28,86 @@ public class LinkSet
 		{ return links; }
 	
 	/**
+	 * Allows you to use LinkSet in a 1.5 for loop.
+	 */
+	public Iterator<Link> iterator()
+		{ return links.iterator(); }
+	
+	/**
 	 * Returns the list of map sets held by this object.
 	 * @return the list of map sets held by this object
 	 */
 	public LinkedList<MapSet> getMapSets()
 		{ return mapSets; }
+	
+	/**
+	 * Returns true if the given link represents a link between two features
+	 * held by maps that are contained within the same map set.
+	 * @param link the link to query
+	 * @return true if the given link represents a link between two features
+	 * held by maps that are contained within the same map set
+	 */
+	public boolean isLinkUniqueToMapSet(Link link)
+	{
+		// Need to find the owning ChromoMap for the two features in this link
+		// and then search the known MapSets to see if they exist in the same
+		// one or not
+		ChromoMap map1 = link.getFeature1().getOwningMap();
+		ChromoMap map2 = link.getFeature2().getOwningMap();
+		
+		// Search for a case where a MapSet holds both the ChromoMaps
+		for (MapSet mapset: mapSets)
+			if (mapset.contains(map1) && mapset.contains(map2))
+				return true;
+		
+		// If it wasn't found, then we can assume the Link is between MapSets
+		return false;
+	}
+	
+	/**
+	 * Returns a new link set that only contains links that join objects located
+	 * in different map sets. Links between objects in the same map set will be
+	 * ignored and not added to the new set. If the original link set only
+	 * contains a single map set, the new link set object will be empty.
+	 */
+	public LinkSet getBetweenMapSetLinks()
+	{
+		LinkSet newSet = new LinkSet();
+		
+		// If there is only 0 or 1 MapSet, then we can't have any between map
+		// links
+		if (mapSets.size() < 2)
+			return newSet;
+		
+		for (Link link: links)
+		{
+			// If the link spans map sets, then we want to add it
+			if (isLinkUniqueToMapSet(link) == false)
+			{
+				newSet.addLink(link);
+				
+				// But we also need to find the map sets involved
+				ChromoMap map1 = link.getFeature1().getOwningMap();
+				ChromoMap map2 = link.getFeature2().getOwningMap();
+				
+				// And add them to the link set too
+				newSet.addMapSet(getMapSet(map1));
+				newSet.addMapSet(getMapSet(map2));
+			}
+		}
+		
+		return newSet;
+	}
+	
+	// Returns the map set that holds the given map
+	private MapSet getMapSet(ChromoMap map)
+	{
+		for (MapSet mapset: mapSets)
+			if (mapset.contains(map))
+				return mapset;
+		
+		return null;
+	}
 	
 	/**
 	 * Adds another map set to this link set.
---
+++ trunk/src/sbrn/mapviewer/io/CMapLinkImporterTest.java
@@ -0,0 +1,116 @@
+package sbrn.mapviewer.io;
+
+import java.io.*;
+import java.util.*;
+
+import sbrn.mapviewer.data.*;
+
+public class CMapLinkImporterTest
+{
+	public static void main(String[] args)
+	{
+		CMapLinkImporter links = new CMapLinkImporter(
+			new File("D:\\Projects\\MapViewer\\cmap formatted files\\simple-corr.txt"));
+		CMapImporter importer = null;
+		
+		try
+		{
+			long s = System.currentTimeMillis();
+						
+			importer = new CMapImporter(new File("D:\\Projects\\MapViewer\\cmap formatted files\\simple-map12.txt"));			
+			MapSet mapSet1 = importer.loadMapSet();
+			mapSet1.printSummary();
+			links.addMapSet(mapSet1);
+			
+			importer = new CMapImporter(new File("D:\\Projects\\MapViewer\\cmap formatted files\\simple-map23.txt"));			
+			MapSet mapSet2 = importer.loadMapSet();
+			mapSet2.printSummary();
+			links.addMapSet(mapSet2);
+			
+			long e = System.currentTimeMillis();
+			System.out.println("Time to load data: " + (e-s) + "ms");
+			
+			s = System.currentTimeMillis();
+			LinkSet linkSet = links.loadLinkSet();
+			e = System.currentTimeMillis();
+			System.out.println("Time to associate links: " + (e-s) + "ms");
+			
+			System.out.println(linkSet);
+						
+			System.out.println("LinkSet contains " + linkSet.getLinks().size() + " links");
+			
+			
+			for (Link link: linkSet)
+			{
+				System.out.println(link);
+				System.out.println("  " + linkSet.isLinkUniqueToMapSet(link));
+			}
+			
+			System.out.println();
+			
+			LinkSet newSet = linkSet.getBetweenMapSetLinks();
+			for (Link link: newSet)
+			{
+				System.out.println(link);
+			}
+		
+		}
+		catch (Exception e) { System.out.println(e); }
+	}
+		
+	private static void cmapTest(String[] args)
+	{
+		CMapLinkImporter links = new CMapLinkImporter(new File(args[0]));
+		CMapImporter importer = null;
+		
+		try
+		{
+			long s = System.currentTimeMillis();
+			
+			Runtime rt = Runtime.getRuntime();
+			System.out.println("Mem = " + (rt.totalMemory() - rt.freeMemory()));
+			
+			importer = new CMapImporter(new File("D:\\Projects\\MapViewer\\cmap formatted files\\new_mxb_edited.maps"));			
+			MapSet mapSet1 = importer.loadMapSet();
+			mapSet1.printSummary();
+			links.addMapSet(mapSet1);
+			
+			importer = new CMapImporter(new File("D:\\Projects\\MapViewer\\cmap formatted files\\new_owb_edited.maps"));			
+			MapSet mapSet2 = importer.loadMapSet();
+			mapSet2.printSummary();
+			links.addMapSet(mapSet2);
+			
+			importer = new CMapImporter(new File("D:\\Projects\\MapViewer\\cmap formatted files\\new_sxm_edited.maps"));			
+			MapSet mapSet3 = importer.loadMapSet();
+			mapSet3.printSummary();
+			links.addMapSet(mapSet3);
+			
+			System.out.println("Mem = " + (rt.totalMemory() - rt.freeMemory()));
+			
+			importer = new CMapImporter(new File("D:\\Projects\\MapViewer\\cmap formatted files\\rice_pseudo4_os.maps"));			
+			MapSet mapSet4 = importer.loadMapSet();
+			mapSet4.printSummary();
+			links.addMapSet(mapSet4);
+			
+			System.out.println("Mem = " + (rt.totalMemory() - rt.freeMemory()));
+			
+			long e = System.currentTimeMillis();
+			System.out.println("Time to load data: " + (e-s) + "ms");
+			
+			s = System.currentTimeMillis();
+			LinkSet linkSet = links.loadLinkSet();
+			e = System.currentTimeMillis();
+			System.out.println("Time to associate links: " + (e-s) + "ms");
+			
+			System.out.println(linkSet);
+			
+			System.out.println("Mem = " + (rt.totalMemory() - rt.freeMemory()));
+			
+			System.out.println("LinkSet contains " + linkSet.getLinks().size() + " links");
+		
+			
+			
+		}
+		catch (Exception e) { System.out.println(e); }
+	}
+}

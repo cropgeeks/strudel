1cc8bc33829e28fe8c6631a03f87a524094ed5a3
Iain Milne <imilne@users.noreply.github.com>
2008-08-13 10:30:56 +0000

    - NEW: Improved compatibility on OS X and Vista. - NEW: Added required jar files for OS X compatibility (osxadapter.jar) and the scri-commons package. - CHG: Modified the layout of the main window and the lhs control panel to fix some sizing issues. - NEW: Added a run.sh script for OS X and Linux testing.

--- src/sbrn/mapviewer/gui/MapViewer.java
+++ src/sbrn/mapviewer/gui/MapViewer.java
@@ -2,18 +2,39 @@ package sbrn.mapviewer.gui;
 
 import javax.swing.*;
 
+import scri.commons.gui.*;
+
+import apple.dts.samplecode.osxadapter.*;
+
 public class MapViewer
 {
-	
 	public static WinMain winMain;
-	
+
 	public static void main(String[] args)
+	{
+		// OS X: This has to be set before anything else
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Map Viewer");
+
+		new MapViewer();
+	}
+
+	MapViewer()
 	{
 		try
 		{
-			// Set System L&F
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			
+			UIManager.put("TextArea.font", UIManager.get("TextField.font"));
+
+			// Keep Apple happy...
+			if (SystemUtils.isMacOS())
+				handleOSXStupidities();
+
+//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		}
+		catch (Exception e) {}
+
+		try
+		{
 			winMain = new WinMain();
 			winMain.setVisible(true);
 		}
@@ -21,5 +42,49 @@ public class MapViewer
 		{
 			e.printStackTrace();
 		}
+	}
+
+	// --------------------------------------------------
+	// Methods required for better native support on OS X
+
+	private void handleOSXStupidities()
+	{
+		try
+		{
+			// Register handlers to deal with the System menu about/quit options
+
+//			OSXAdapter.setPreferencesHandler(this,
+//				getClass().getDeclaredMethod("osxPreferences", (Class[])null));
+
+//			OSXAdapter.setAboutHandler(this,
+//				getClass().getDeclaredMethod("osxAbout", (Class[])null));
+
+			OSXAdapter.setQuitHandler(this,
+				getClass().getDeclaredMethod("osxShutdown", (Class[])null));
+
+			// Dock the menu bar at the top of the screen
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+		}
+		catch (Exception e) {}
+	}
+
+	/** "Preferences" on the OS X system menu. */
+	public void osxPreferences()
+	{
+//		winMain.mHelp.helpPrefs();
+	}
+
+	/** "About Map Viewer" on the OS X system menu. */
+	public void osxAbout()
+	{
+//		winMain.mHelp.helpAbout();
+	}
+
+	/** "Quit Map Viewer" on the OS X system menu. */
+	public boolean osxShutdown()
+	{
+		// Put any additional quit handling code here (if need be)
+
+		return true;
 	}
 }
--- src/sbrn/mapviewer/gui/WinMain.java
+++ src/sbrn/mapviewer/gui/WinMain.java
@@ -9,66 +9,68 @@ public class WinMain extends JFrame
 {
 	//this is where we hold the genome data
 	public DataContainer dataContainer;
-	
+
 	//Swing components that make up the GUI
 	//this is where we paint the genomes
 	public static MainCanvas mainCanvas;
 	public ZoomControlPanel zoomControlPanel;
 	public ControlPanel controlPanel;
 	public AnnotationPanel targetAnnotationPanel;
-	public AnnotationPanel referenceAnnotationPanel;	
+	public AnnotationPanel referenceAnnotationPanel;
 	public OverviewCanvas targetOverviewCanvas;
 	public OverviewCanvas referenceOverviewCanvas;
 	public Scroller leftCanvasScroller;
 	public Scroller rightCanvasScroller;
-	
+
 	//the controller for the whole application
 	FatController fatController;
-	
+
 	public WinMain()
 	{
 		//load the data for testing
 		//TODO remove hard coded data loading once user import options are available
 		dataContainer = new DataContainer();
 		dataContainer.loadData();
-		
+
 		//get the GUI assembled
 		setupComponents();
-		
+		pack();
+
 		//GUI bits and pieces
 		setTitle("Map Viewer");
+		setSize(950, 700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
+
 		setLocationRelativeTo(null);
 //		setExtendedState(this.MAXIMIZED_BOTH);
 	}
-	
+
 	private void setupComponents()
 	{
 		//the fat controller
-		fatController = new FatController(this);		
-		
+		fatController = new FatController(this);
+
 		//this panel contains everything else in a borderlayout
-		JPanel topContainerPanel =  new JPanel(new BorderLayout());		
-		//this panel contains the main canvas, the annotation panel below it, and 
+		JPanel topContainerPanel = new JPanel(new BorderLayout());
+		//this panel contains the main canvas, the annotation panel below it, and
 		//the 2 scrollers left and right of the canvas
-		JPanel mainPanel = new JPanel(new BorderLayout());		
+		JPanel mainPanel = new JPanel(new BorderLayout());
 		//this panel contains the two overview panels and the control panel
 		JPanel leftPanel = new JPanel(new BorderLayout());
 		//a panel for the two overview canvases
 		JPanel overViewsContainer = new JPanel(new GridLayout(1,2));
-		
+
 		//scroll bars for the canvas
 		leftCanvasScroller = new Scroller(this);
 		rightCanvasScroller = new Scroller(this);
 		mainPanel.add(leftCanvasScroller,BorderLayout.WEST);
 		mainPanel.add(rightCanvasScroller,BorderLayout.EAST);
-		
+
 		//this is the main canvas which we render the genomes on
 		mainCanvas = new MainCanvas(dataContainer.targetMapset, dataContainer.referenceMapset, this, dataContainer.links);
 		mainPanel.add(mainCanvas, BorderLayout.CENTER);
-		mainCanvas.setPreferredSize(new Dimension(800, 600));
-		
+//		mainCanvas.setPreferredSize(new Dimension(800, 600));
+
 		//add mousehandler
 		MouseHandler mouseHandler = new MouseHandler(this);
 		mainCanvas.addMouseListener(mouseHandler);
@@ -76,36 +78,36 @@ public class WinMain extends JFrame
 		mainCanvas.addMouseWheelListener(mouseHandler);
 		leftCanvasScroller.addMouseListener(mouseHandler);
 		rightCanvasScroller.addMouseListener(mouseHandler);
-		
+
 		//the panel with the zoom control sliders
 		zoomControlPanel = new ZoomControlPanel(this);
 		//zoomControlPanel.setPreferredSize(new Dimension(800, 100));
 //		add(zoomControlPanel,BorderLayout.SOUTH);
-		
+
 		//the panels for displaying annotation info
 		targetAnnotationPanel = new AnnotationPanel(this,dataContainer.targetMapset);
 		referenceAnnotationPanel = new AnnotationPanel(this,dataContainer.referenceMapset);
 		JPanel annotationPanelContainer = new JPanel(new GridLayout(1,2));
 		annotationPanelContainer.add(targetAnnotationPanel);
-		annotationPanelContainer.add(referenceAnnotationPanel);	
-		annotationPanelContainer.setPreferredSize(new Dimension(800, 100));
+		annotationPanelContainer.add(referenceAnnotationPanel);
+//		annotationPanelContainer.setPreferredSize(new Dimension(800, 100));
 		mainPanel.add(annotationPanelContainer,BorderLayout.SOUTH);
 
-		//the control panel 
+		//the control panel
 		controlPanel = new ControlPanel(this);
-		controlPanel.setPreferredSize(new Dimension(230,400));
-		
+
 		//the overviews for the two genomes
 		targetOverviewCanvas = new OverviewCanvas(this,mainCanvas.targetGMapSet);
 		referenceOverviewCanvas = new OverviewCanvas(this,mainCanvas.referenceGMapSet);
-		targetOverviewCanvas.setPreferredSize(new Dimension(100,250));
-		referenceOverviewCanvas.setPreferredSize(new Dimension(100,220));
+		targetOverviewCanvas.setPreferredSize(new Dimension(0,250));
+		referenceOverviewCanvas.setPreferredSize(new Dimension(0,220));
 		overViewsContainer.add(targetOverviewCanvas);
-		overViewsContainer.add(referenceOverviewCanvas);		
-		
+		overViewsContainer.add(referenceOverviewCanvas);
+
 		//put it all together
 		leftPanel.add(overViewsContainer,BorderLayout.NORTH);
-		leftPanel.add(controlPanel,BorderLayout.CENTER);		
+		leftPanel.add(controlPanel,BorderLayout.CENTER);
+
 		topContainerPanel.add(leftPanel, BorderLayout.WEST);
 		topContainerPanel.add(mainPanel, BorderLayout.CENTER);
 		this.add(topContainerPanel);

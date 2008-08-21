10d95a6dff5cd00a2c12cf767d1e602846a5f849
imilne <imilne@75c49e0a-a916-0410-83a6-e98dbbb37169>
2008-08-21 08:06:46 +0000

    - NEW: Added event tracking code so that the application window's position (x,y), size, and maximization state will be remembered between sessions.

--- trunk/src/sbrn/mapviewer/gui/Prefs.java
+++ trunk/src/sbrn/mapviewer/gui/Prefs.java
@@ -8,4 +8,14 @@ public class Prefs extends XMLPreferences
 {
 	// Unique user ID for this user
 	public static String ID = SystemUtils.createGUID(32);
+
+	// Is this the first time the program has ever been run (by this user)?
+	public static boolean isFirstRun = true;
+
+	// The width, height, location and maximized status of the main window
+	public static int guiWinMainWidth = 800;
+	public static int guiWinMainHeight = 600;
+	public static int guiWinMainX = 0;
+	public static int guiWinMainY = 0;
+	public static boolean guiWinMainMaximized = false;
 }

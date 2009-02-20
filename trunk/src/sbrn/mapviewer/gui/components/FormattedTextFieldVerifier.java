7b28b0ef6223902ac11dd08e40792c16585e7e0c
mbayer <mbayer@75c49e0a-a916-0410-83a6-e98dbbb37169>
2009-02-20 13:52:41 +0000

    - NEW: Added an input verifier to the spinner textfields in the range dialog. This forces the inputs to be positive numbers.

---
+++ trunk/src/sbrn/mapviewer/gui/components/FormattedTextFieldVerifier.java
@@ -0,0 +1,36 @@
+package sbrn.mapviewer.gui.components;
+
+import java.text.*;
+import javax.swing.*;
+import javax.swing.JFormattedTextField.*;
+import scri.commons.gui.*;
+
+public class FormattedTextFieldVerifier extends InputVerifier
+{
+	public boolean verify(JComponent input)
+	{
+		if (input instanceof JFormattedTextField)
+		{
+			JFormattedTextField ftf = (JFormattedTextField) input;
+			AbstractFormatter formatter = ftf.getFormatter();
+			if (formatter != null)
+			{
+				String text = ftf.getText();
+
+					if(Float.parseFloat(text) > 0)
+						return true;
+					else
+					{
+						TaskDialog.error("This number must be positive.", "Close");
+						return false;
+					}
+			}
+		}
+		return true;
+	}
+	
+	public boolean shouldYieldFocus(JComponent input)
+	{
+		return verify(input);
+	}
+}
